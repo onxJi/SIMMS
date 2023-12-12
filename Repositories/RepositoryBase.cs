ab89394bbf695a7b73f8f867eac284bb0711c5ae
onxJi <jaaziel-jz77@outlook.com>
2023-12-11 22:54:53 -0700

    Actualizacion

--- Repositories/RepositoryBase.cs
+++ Repositories/RepositoryBase.cs
@@ -14,12 +14,11 @@ namespace SIMMS.Repositories
         private string cn;
         public RepositoryBase()
         {
-            //connection = "Server=tcp:DESKTOP-QGU951C\\ONXCORP,7680;DataBase=sySIMMS; User Id=sa; Password=OnxJi090701";
+          
             //connection = "8D39SUiHTqv3qc/qr0uGH20f1O8o7jG9DAMHWSxTdnFBctuq2Ntc2xP" +
             //    "0JmRnfonBHKv+PASSZTJ2flSkOj5U6AWwUhFP7HnucGiG1JtYblA5bwP3cIvn6UEAPknBb0Wr";
             connection = "Server=ONX;Database=sySIMMS;Integrated Security=true";
-            //connection = "MzfSduawXEzilGBEyWkM4y3jlMDw5e+EzUrZL5RVp/3a9U5mptDzFsjnXCz1RFRkx3AhpQJfNm5Aj74TAkEp++yKYZ/I0f977nt3wryk8BI=";
-            //connection = Convert.ToString(Descrypt.checkServer());
+            
 
         }
         protected SqlConnection GetSqlConnection()
